7350bd6db5cb7829a087c1c44e1b31cf9f3fd7cf
Crims91 <cucerst@gmail.com>
2023-04-04 21:52:26 +0300

    almost finished

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ window.addEventListener("load", function () {
   let playerCounter = 1;
   let computerCounter = 1;
 
+  const playerScoreDiv = document.getElementById("player-score");
+  const computerScoreDiv = document.getElementById("computer-score");
+  const playerImg = document.querySelector(".user-hands img");
+  const computerImg = document.querySelector(".computer-hands img");
+  const handsBlock = document.querySelector(".hands-block");
+  const resetBlock = document.querySelector(".reset");
+  const rpsButtons = document.querySelectorAll(".card");
+  const resultDiv = document.getElementById("result");
+
   const getComputerChoice = () => {
     const rpsChoice = ["Rock", "Paper", "Scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
+
     return rpsChoice[randomNumber];
   };
 
@@ -31,11 +41,6 @@ window.addEventListener("load", function () {
   };
 
   const showResult = () => {
-    const resultDiv = document.getElementById("result");
-    const handsDiv = document.getElementById("hands-block");
-    const playerScoreDiv = document.getElementById("player-score");
-    const computerScoreDiv = document.getElementById("computer-score");
-
     const { playerScore, computerScore } = totalScore;
 
     if (playerCounter === playerScore) {
@@ -55,12 +60,19 @@ window.addEventListener("load", function () {
     computerScoreDiv.innerText = `Computer score: ${computerScore}`;
   };
 
+  const updateHandsImg = (playerChoice, computerChoice) => {
+    playerImg.src = `img/${playerChoice.toLowerCase()}.png`;
+    computerImg.src = `img/${computerChoice.toLowerCase()}.png`;
+  };
+
   const onClickRps = (playerChoice) => {
     const computerChoice = getComputerChoice();
     const { userScore, computerScore } = getResult(
       playerChoice,
       computerChoice
     );
+
+    updateHandsImg(playerChoice, computerChoice);
 
     if (userScore > 0) {
       totalScore.playerScore += 1;
@@ -76,13 +88,37 @@ window.addEventListener("load", function () {
   };
 
   const playGame = () => {
-    const rpsButtons = document.querySelectorAll(".card");
-    rpsButtons[0].onclick = () => console.log(rpsButtons[0].value);
-
     rpsButtons.forEach((rpsButton) => {
       rpsButton.onclick = () => onClickRps(rpsButton.value);
     });
   };
+
+  const resetGame = () => {
+    totalScore.playerScore = 0;
+    totalScore.computerScore = 0;
+    playerCounter = 1;
+    computerCounter = 1;
+
+    handsBlock.style.display = "none";
+    resetBlock.style.display = "none";
+
+    playerScoreDiv.innerText = `Your score: 0`;
+    computerScoreDiv.innerText = `Computer score: 0`;
+    playerImg.src = "img/paper.png";
+    computerImg.src = "img/paper.png";
+    resultDiv.innerHTML = "&nbsp;";
+  };
+
+  document.getElementById("reset-button").addEventListener("click", resetGame);
+
+  // To display the hands text, images and Play again button after I choose my card
+
+  rpsButtons.forEach((card) => {
+    card.addEventListener("click", () => {
+      handsBlock.style.display = "flex";
+      resetBlock.style.display = "flex";
+    });
+  });
 
   playGame();
 });
